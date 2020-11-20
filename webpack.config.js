e2addf1ef0aae78fb02811609fd529699905d05e
yaguruma <yaguruma_y@yahoo.co.jp>
2020-11-20 19:55:27 +0900

    change config html-webpack-plugin: inject body->head

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,8 @@ module.exports = {
         }),
         new HtmlPlugin({
             template: './src/index.html',
-            inject: 'body'
-        })
+            inject: 'head',
+            scriptLoading: 'defer',
+        }),
     ],
 };
