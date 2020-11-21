f6db75cd1205436b0cba2a23338c7cf6a633d5e3
yaguruma <yaguruma_y@yahoo.co.jp>
2020-11-21 14:36:12 +0900

    pixi.jsでHelloWorld

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello, Slot Game!');
+import * as PIXI from 'pixi.js';
+
+const app = new PIXI.Application({ width: 400, height: 200 });
+document.body.append(app.view);
+
+const text = new PIXI.Text('Hello, World!');
+text.style.fill = '#ffffff';
+app.stage.addChild(text);
