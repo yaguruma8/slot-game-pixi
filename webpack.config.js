898873dadaa1174e30605e2911e484969a61d3e6
yaguruma <yaguruma_y@yahoo.co.jp>
2020-11-23 20:40:39 +0900

    entryをindex.tsに戻す

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: { app: './src/index.js' },
+    entry: { app: './src/index.ts' },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js',
